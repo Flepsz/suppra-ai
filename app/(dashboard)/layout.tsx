9bb3968d9a7e27aaf10c65e7a8a759a1f72df8cb
Felipe Pereira <luisfelipelfsp3@gmail.com>
2023-12-17 23:03:37 -0300

    :sparkles: feat: Changed bgColor

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -8,7 +8,7 @@ export default function DashboardLayout({
 }) {
 	return (
 		<div className="relative h-full">
-      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
+      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-[#080c14]">
         <div>
           <Sidebar />
         </div>
